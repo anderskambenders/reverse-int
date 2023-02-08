de53b180bf2876100bc0fd208d37f00a841840f1
anderskambenders <100037401+anderskambenders@users.noreply.github.com>
2023-02-08 13:40:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let numString = n.toString();
+    let newNumString = '';
+    for (let i = numString.length - 1; i >= 0 ; --i) {
+        newNumString += numString[i];
+    }
+    return Number(newNumString.replace('-', ''))
+
 }
